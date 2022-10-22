8f66a396b2573980ae937486c2a073c23a51b2c7
sebastianiv21 <seluti98@gmail.com>
2022-10-21 21:48:36 -0500

    Agregados registros de tablas informes y clientes

--- client/src/pages/ListadoInformes.jsx
+++ client/src/pages/ListadoInformes.jsx
@@ -1,99 +1,102 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload, faTrashCan } from "@fortawesome/free-solid-svg-icons";
-import { Pagination, PaginationItem, PaginationLink, Table } from "reactstrap";
+import { useState } from 'react'
+import { faDownload, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { Pagination, PaginationItem, PaginationLink, Table } from 'reactstrap'
+import Boton from '../components/Boton'
+import FilaInforme from '../components/FilaInforme'
+import ListadoInformesData from '../shared/ListadoInformesData'
 
 const ListadoInformes = () => {
+  const [informes, setInformes] = useState(ListadoInformesData)
+
+  const listaInforme = informes.map((item) => (
+    <FilaInforme
+      compania={item.compania}
+      fechaCreacion={item.fechaCreacion}
+      key={item.id}
+    />
+  ))
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="bg-primary d-flex p-2 position-relative rounded-top mt-3 align-items-center">
+    <div className='container'>
+      <div className='row'>
+        <div className='bg-primary d-flex p-2 position-relative rounded-top mt-3 align-items-center'>
           {/* <!--Primera fila--> */}
-          <button className="btn btn-secondary text-primary me-2 ms-2">
-            <FontAwesomeIcon icon={faDownload} />
-          </button>
-          <button className="btn btn-secondary text-primary">
-            <FontAwesomeIcon icon={faTrashCan} />
-          </button>
-          <h5 className="text-white m-0 mx-auto">34 informes</h5>
+
+          <Boton
+            icono={faDownload}
+            estilos='me-2 ms-2'
+            colorBtn='secondary'
+            colorTxt='primary'
+          />
+          <Boton
+            icono={faTrashCan}
+            colorBtn='secondary'
+            colorTxt='primary'
+          />
+          <h5 className='text-white m-0 mx-auto'>34 informes</h5>
         </div>
-        <div className="p-0">
+        <div className='p-0'>
           {/* <table className="table table-bordered table-hover text-center"> */}
           <Table
-  bordered
-  hover
-  responsive
-  striped
-  className="text-center"
->
-            <thead className="bg-primary text-white">
+            bordered
+            hover
+            responsive
+            striped
+            className='text-center'
+          >
+            <thead className='bg-primary text-white'>
               <tr>
                 <th></th>
                 <th>
-                  <label htmlFor="compania" className="form-label text-center">
+                  <label
+                    htmlFor='compania'
+                    className='form-label text-center'
+                  >
                     Compañía
                   </label>
-                  <div className="mx-auto">
+                  <div className='mx-auto'>
                     <select
-                      name="compania"
-                      id="compania"
-                      className="form-select"
+                      name='compania'
+                      id='compania'
+                      className='form-select'
                     >
-                      <option selected value="" disabled>
+                      <option
+                        selected
+                        value=''
+                        disabled
+                      >
                         Seleccionar...
                       </option>
-                      <option value="FAM">Fundacion alto magdalena</option>
-                      <option value="varisur">Varisur</option>
-                      <option value="LCC">Liga contra el cancer</option>
+                      <option value='FAM'>Fundacion alto magdalena</option>
+                      <option value='varisur'>Varisur</option>
+                      <option value='LCC'>Liga contra el cancer</option>
                     </select>
                   </div>
                 </th>
                 <th>
                   <label
-                    htmlFor="fechaCreacion"
-                    className="form-label text-center"
+                    htmlFor='fechaCreacion'
+                    className='form-label text-center'
                   >
                     Fecha de creación
                   </label>
-                  <div className="mx-auto">
+                  <div className='mx-auto'>
                     <input
-                      type="date"
-                      name="fechaCreacion"
-                      id="fechaCreacion"
-                      className="form-control"
+                      type='date'
+                      name='fechaCreacion'
+                      id='fechaCreacion'
+                      className='form-control'
                     />
                   </div>
                 </th>
               </tr>
             </thead>
-            <tbody className="bg-secondary">
-              <tr>
-                <td>
-                  <input type="checkbox" name="" id="" />
-                </td>
-                <td>
-                  <p className="m-0">IT TECHNOLOGY</p>
-                </td>
-                <td>
-                  <p className="m-0">21-Abril-2022</p>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <input type="checkbox" name="" id="" />
-                </td>
-                <td>
-                  <p className="m-0">IT TECHNOLOGY</p>
-                </td>
-                <td>
-                  <p className="m-0">30-Agosto-2022</p>
-                </td>
-              </tr>
-            </tbody>
-          {/* </table> */}
+            <tbody className='bg-secondary'>{listaInforme}</tbody>
+            {/* </table> */}
           </Table>
         </div>
       </div>
-      <div className="d-flex justify-content-center">
+      <div className='d-flex justify-content-center'>
         {/* <!--Botones--> */}
         {/* <div className="mx-auto">
           <button className="btn btn-primary text-white mx-1">1</button>
@@ -105,36 +108,77 @@ const ListadoInformes = () => {
         </div> */}
         <Pagination>
           <PaginationItem>
-            <PaginationLink first href="#" className="bg-primary text-white" />
+            <PaginationLink
+              first
+              href='#'
+              className='bg-primary text-white'
+            />
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#" previous className="bg-primary text-white"  />
+            <PaginationLink
+              href='#'
+              previous
+              className='bg-primary text-white'
+            />
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#" className="bg-primary text-white" >1</PaginationLink>
+            <PaginationLink
+              href='#'
+              className='bg-primary text-white'
+            >
+              1
+            </PaginationLink>
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#" className="bg-primary text-white" >2</PaginationLink>
+            <PaginationLink
+              href='#'
+              className='bg-primary text-white'
+            >
+              2
+            </PaginationLink>
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#" className="bg-primary text-white" >3</PaginationLink>
+            <PaginationLink
+              href='#'
+              className='bg-primary text-white'
+            >
+              3
+            </PaginationLink>
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#" className="bg-primary text-white" >4</PaginationLink>
+            <PaginationLink
+              href='#'
+              className='bg-primary text-white'
+            >
+              4
+            </PaginationLink>
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#" className="bg-primary text-white" >5</PaginationLink>
+            <PaginationLink
+              href='#'
+              className='bg-primary text-white'
+            >
+              5
+            </PaginationLink>
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#" next className="bg-primary text-white"  />
+            <PaginationLink
+              href='#'
+              next
+              className='bg-primary text-white'
+            />
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#" last className="bg-primary text-white"  />
+            <PaginationLink
+              href='#'
+              last
+              className='bg-primary text-white'
+            />
           </PaginationItem>
         </Pagination>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ListadoInformes;
+export default ListadoInformes
