d0530cbb8724369ec96322b916d27bc58ea74b33
sebastianiv21 <seluti98@gmail.com>
2022-11-28 21:07:57 -0500

    agregado modelo de usuario

---
+++ server/models/Usuario.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const usuarioSchema = new mongoose.Schema({
+  usuario: {
+    type: String,
+    required: true,
+  },
+  contrasenha: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('Usuario', usuarioSchema)
