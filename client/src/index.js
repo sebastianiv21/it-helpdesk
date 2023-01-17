69139e135fcf00c998802d6278d9910cc3c07ff1
sebastianiv21 <seluti98@gmail.com>
2023-01-17 16:01:03 -0500

    agregado scss como dependencia

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React, {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-// eslint-disable-next-line
-import bootstrap from 'bootstrap';
 import App from './App';
+import './custom.scss'
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
