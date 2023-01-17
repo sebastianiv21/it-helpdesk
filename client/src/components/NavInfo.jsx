c8047d3dfc53f379665164ca2c51abd0a8ce3b3f
sebastianiv21 <seluti98@gmail.com>
2023-01-17 14:28:36 -0500

    pequeños cambios a ruta de icono en navbar

--- client/src/components/NavInfo.jsx
+++ client/src/components/NavInfo.jsx
@@ -7,7 +7,7 @@ const NavInfo = () => {
         to="/"
         className="d-flex align-items-center text-primary text-decoration-none ms-4 me-auto"
       >
-        <img src="./images/logo.svg" alt="IT Technology logo" width="40" />
+        <img src="/images/logo.svg" alt="IT Technology logo" width="40" />
         <div className="ms-2 fs-5 lh-1">
           <p className="fw-bold m-0">IT TECHNOLOGY</p>
           <p className="m-0">Help Desk</p>
