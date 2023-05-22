461b2a5959e8325ccb17cc0829526c1516e448b0
Alvaro Jose <ajosep108816@gmail.com>
2023-05-22 08:19:50 -0500

    contenido y funcionamiento de ver y generar pdf

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,2 +1,3 @@
 export * from './useForm'
 export * from './useData'
+export * from './useDate'
---
+++ client/src/hooks/useDate.js
@@ -0,0 +1,31 @@
+import { format, parseISO, formatISO } from 'date-fns'
+import { es } from 'date-fns/locale'
+
+export const useDate = () => {
+  const parseDate = (date) => {
+    return format(parseISO(date), 'dd-MM-yyyy')
+  }
+
+  const parseDateString = (date, type) => {
+    if (type === 'date-only') {
+      const formatedDate = format(new Date(date), 'dd MMMM yyyy', { locale: es })
+      return formatedDate
+    } else if (type === 'date-hours') {
+      const formatedDate = format(new Date(date), 'dd MMMM yyyy HH:mm:ss', { locale: es })
+      return formatedDate
+    }
+  }
+
+  const actualDateForInputs = () => { // Limits days of input until today
+    const fecha = new Date()
+    return formatISO(fecha, { representation: 'date' })
+  }
+
+  const isFutureDate = (date) => { // Validates if date selected is > actual day
+    const today = new Date()
+    const parsedDate = parseISO(date)
+    return parsedDate > today
+  }
+
+  return { parseDate, isFutureDate, parseDateString, actualDateForInputs }
+}
