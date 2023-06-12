4b45bb50f249d33e51ef7f791552cfa0b9924646
sebastianiv21 <seluti98@gmail.com>
2023-06-11 20:19:21 -0500

    feat(FRONTEND): :sparkles: ahora es posible acceder a edicion de ticket desde inicio

--- client/src/pages/Inicio.jsx
+++ client/src/pages/Inicio.jsx
@@ -1,35 +1,99 @@
 import { useState, useEffect } from 'react'
-import { useData } from '@hooks'
-import { Container, Row, Col, Card, CardHeader, CardBody } from 'reactstrap'
+import { useData, useToggle } from '@hooks'
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  Modal,
+  ModalHeader
+} from 'reactstrap'
+import { toast } from 'react-toastify'
 import GraficoBarras from '../components/GraficoBarras'
+import EditarTicket from '@components/EditarTicket'
 import { TablaInicio } from '../components/TablaInicio'
+import axios from '../api/axios'
 
 const Inicio = () => {
   const { getTickets, countObjectsWithPropertyValue } = useData()
   const [pendientes, setPendientes] = useState(0)
   const [prioritarios, setPrioritarios] = useState([])
   const [tickets, setTickets] = useState([])
+  const [isEditOpen, toggleEdit] = useToggle()
+  const [currentTicket, setCurrentTicket] = useState(null)
 
+  const TICKETS_URL = '/tickets'
   const hayPrioritarios = Boolean(prioritarios.length)
 
+  const getTicketsInfo = async () => {
+    const json = await getTickets()
+    const [pendientesArr, pendientes] = countObjectsWithPropertyValue(
+      json,
+      'estado',
+      'Abierto'
+    )
+    const [prioritariosArr] = countObjectsWithPropertyValue(
+      pendientesArr,
+      'prioridad',
+      'Alta'
+    )
+    setTickets(json)
+    setPendientes(pendientes)
+    setPrioritarios(prioritariosArr)
+  }
+
   useEffect(() => {
-    getTickets().then((json) => {
-      const [pendientesArr, pendientes] = countObjectsWithPropertyValue(
-        json,
-        'estado',
-        'Abierto'
+    getTicketsInfo()
+  }, [])
+
+  const onUpdate = async (formData) => {
+    if (formData?.estado === 'Cerrado' && !formData?.fechadecierre) {
+      toast.error('Ingrese la fecha de cierre', {
+        theme: 'colored'
+      })
+      return
+    }
+
+    try {
+      await axios.patch(TICKETS_URL, formData)
+      toast.info('Ticket actualizado exitosamente', {
+        theme: 'colored'
+      })
+      const ticketIndex = searchResults.findIndex(
+        (ticket) => ticket._id === formData._id
       )
-      const [prioritariosArr] = countObjectsWithPropertyValue(
-        pendientesArr,
-        'prioridad',
-        'Alta'
-      )
-      setTickets(json)
-      setPendientes(pendientes)
-      setPrioritarios(prioritariosArr)
-      return [pendientes, prioritariosArr, json]
-    })
-  }, [getTickets, countObjectsWithPropertyValue])
+
+      setSearchResults((prevItems) => {
+        const updatedItems = [...prevItems]
+        updatedItems[ticketIndex] = formData
+        return updatedItems
+      })
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg('El servidor no responde')
+      } else if (err.response?.status === 400) {
+        setErrMsg('Ingrese todos los campos del formulario')
+      } else {
+        setErrMsg('La actualización del ticket falló')
+      }
+    } finally {
+      toggleEdit()
+      getTicketsInfo()
+    }
+  }
+
+  const handleEditToggle = (ticket) => {
+    setCurrentTicket(ticket)
+    toggleEdit()
+  }
+
+  const editarTicketProps = {
+    ticket: currentTicket,
+    onUpdate,
+    toggleEdit
+  }
 
   return (
     <Container fluid className='vh-100 d-flex flex-column' id='inicio'>
@@ -69,12 +133,24 @@ const Inicio = () => {
                     : 'indicadorNumero text-primary'
                 }
               >
-                {hayPrioritarios && <TablaInicio items={prioritarios} />}
+                {hayPrioritarios && (
+                  <TablaInicio
+                    items={prioritarios}
+                    handleEditToggle={handleEditToggle}
+                  />
+                )}
                 {!hayPrioritarios && <strong>{prioritarios.length}</strong>}
               </CardBody>
             </Card>
           </Col>
         </Row>
+        {/* modal de edicion */}
+        <Modal centered fullscreen isOpen={isEditOpen} toggle={toggleEdit}>
+          <ModalHeader toggle={toggleEdit}>
+            Editar ticket {currentTicket?.ticketRef}
+          </ModalHeader>
+          <EditarTicket {...editarTicketProps} />
+        </Modal>
       </Container>
     </Container>
   )
